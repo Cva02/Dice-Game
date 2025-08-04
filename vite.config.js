8db84d98decbe7f5459580d5bcdf16445dbea80e
Siva Kumar Koyyapu <sivakumarkoyyapu8@gmail.com>
2025-08-04 12:50:19 +0530

    ensure vite base path set for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Set base path conditionally
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Dice-Game/' : '/',
+  base: process.env.VERCEL ? '/' : '/Dice-Game/',
   plugins: [react()],
 })
